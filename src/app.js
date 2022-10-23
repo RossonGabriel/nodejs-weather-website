0691b49f433fbfcaefdc6c8d22877ccbae369249
Nguyen Tuong Nhat Quang <quang.nguyentuongnhat@gmail.com>
2022-10-23 23:13:23 +0700

    Add hello text

--- src/app.js
+++ src/app.js
@@ -1,102 +1,110 @@
-const path = require('path')
-const express = require('express')
-const hbs = require('hbs')
-const geocode = require('./utils/geocode')
-const forecast = require('./utils/forecast')
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
-const app = express()
-const port = process.env.PORT || 3000
-
+const app = express();
+const port = process.env.PORT || 3000;
+// Hello
 // Define paths for Express config
-const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates/views')
-const partialsPath = path.join(__dirname, '../templates/partials')
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 // Setup handlebars engine and views location
-app.set('view engine', 'hbs')
-app.set('views', viewsPath)
-hbs.registerPartials(partialsPath)
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve
-app.use(express.static(publicDirectoryPath))
+app.use(express.static(publicDirectoryPath));
 
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather App',
-        name: 'Quang Nhat'
-    })
-})
+        name: 'Quang Nhat',
+    });
+});
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Quang Nhat'
-    })
-})
+        name: 'Quang Nhat',
+    });
+});
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        helpText: 'Just type a city name and you will see its weather forecast. Have fun!!!',
+        helpText:
+            'Just type a city name and you will see its weather forecast. Have fun!!!',
         title: 'Help',
-        name: 'Quang Nhat'
-    })
-})
+        name: 'Quang Nhat',
+    });
+});
 
 app.get('/weather', (req, res) => {
     if (!req.query.address) {
         return res.send({
-            error: 'You must provide an address!'
-        })
+            error: 'You must provide an address!',
+        });
     }
 
-    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
-        if (error) {
-            return res.send({ error })
-        }
-
-        forecast(latitude, longitude, (error, forecastData, weatherForecastImg) => {
+    geocode(
+        req.query.address,
+        (error, { latitude, longitude, location } = {}) => {
             if (error) {
-                return res.send({ error })
+                return res.send({ error });
             }
 
-            res.send({
-                forecast: forecastData,
-                location,
-                weatherForecastImg
-            })
-        })
-    })
-})
+            forecast(
+                latitude,
+                longitude,
+                (error, forecastData, weatherForecastImg) => {
+                    if (error) {
+                        return res.send({ error });
+                    }
+
+                    res.send({
+                        forecast: forecastData,
+                        location,
+                        weatherForecastImg,
+                    });
+                }
+            );
+        }
+    );
+});
 
 app.get('/products', (req, res) => {
     if (!req.query.search) {
         return res.send({
-            error: 'You must provide a search term'
-        })
+            error: 'You must provide a search term',
+        });
     }
 
-    console.log(req.query.search)
+    console.log(req.query.search);
     res.send({
-        products: []
-    })
-})
+        products: [],
+    });
+});
 
 app.get('/help/*', (req, res) => {
     res.render('404', {
         errorMessage: 'Help article not found!',
         title: '404',
-        name: 'Quang Nhat'
-    })
-})
+        name: 'Quang Nhat',
+    });
+});
 
 app.get('*', (req, res) => {
     res.render('404', {
         errorMessage: 'Page not found',
         title: '404',
-        name: 'Quang Nhat'
-    })
-})
+        name: 'Quang Nhat',
+    });
+});
 
 app.listen(port, () => {
-    console.log('Server is up on port ' + port)
-})
+    console.log('Server is up on port ' + port);
+});
